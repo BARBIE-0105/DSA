ba279eb942d591235397cc71df3039177bd1e770
Barbie <barbie.ynr@gmail.com>
2026-08-05 19:21:59 +0530

    Time: 62 ms (58.35%) | Memory: 107.7 MB (23.27%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,13 +1,16 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        int[]results=new int[temperatures.length];
-        Stack<Integer>stack=new Stack<>();
-        for(int i=0;i<temperatures.length;i++){
-            while(!stack.isEmpty()&& temperatures[stack.peek()]<temperatures[i]){
-                results[stack.peek()]=i-stack.pop();
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[temperatures.length];
+
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int idx = stack.pop();
+                result[idx] = i - idx;
             }
             stack.push(i);
         }
-        return results;
+
+        return result;        
     }
 }
